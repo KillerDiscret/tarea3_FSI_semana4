29a71aa7f6f0370e1e6fbd4bdbbe98d032b125fa
KillerDiscret <carlos_p_s_1998@hotmail.com>
2019-04-14 12:47:23 -0500

    Agregar archivos de proyecto.

---
+++ Tarea3_FSI/CArreglo.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tarea3_FSI
+{
+    public class CArreglo
+    {
+        private Array arr_Vendedor = Array.CreateInstance(typeof(Cvendedor), 100);
+        //se usa otra variable para controlar la posicion al ingresar obj
+        private int indice = 0;
+        public int Insertar(Cvendedor obj, int codigo)
+        {
+ 
+            int contador=0;
+            if (arr_Vendedor.GetValue(0) == null)
+            {
+                arr_Vendedor.SetValue(obj, indice);
+                indice++;
+            }
+            else
+            {
+                foreach (Cvendedor elemento in arr_Vendedor)
+                    if (elemento != null)
+                        if (elemento.Codigo == codigo)
+                            contador = 1;
+            }
+            return contador;
+        }
+
+
+
+
+
+    }
+}
---
+++ Tarea3_FSI/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tarea3_FSI
+{
+    public partial class Form1 : Form
+    {
+        CArreglo objarreglo = new CArreglo();
+        Cvendedor objvendedor = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            int auxcodigo = Convert.ToInt32(textBoxCodigo.Text);
+            string auxnombre = textBoxVendedor.Text;
+            int auxmonto = Convert.ToInt32(textBoxVentas.Text);
+            objvendedor = new Cvendedor(auxcodigo, auxnombre, auxmonto);
+            if(objarreglo.Insertar(objvendedor,auxcodigo)==1)
+            {
+                MessageBox.Show("mal");
+            }
+            else
+            {
+                listBoxCodigo.Items.Add(auxcodigo.ToString());
+                listBoxVendedor.Items.Add(auxnombre);
+                listBoxVentas.Items.Add(auxnombre.ToString());
+            }
+
+        }
+    }
+}
